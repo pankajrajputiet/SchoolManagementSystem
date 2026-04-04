31a963b70efbcbfe06edd6d775f7894bdaef7006
pankajrajputiet <pankaj.rajput.iet@GMAIL.COM>
2026-04-04 10:35:38 +0530

    updated

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const logger = require('./logger');
-const MONGODB_URI = "mongodb+srv://pankajrajputiet_db_user:YzDqHObMtjXOLB7A@cluster0.sxzxzrc.mongodb.net/school_management?retryWrites=true&w=majority";
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://pankajrajputiet_db_user:YzDqHObMtjXOLB7A@cluster0.sxzxzrc.mongodb.net/school_management?retryWrites=true&w=majority");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     logger.info(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     logger.error(`MongoDB connection error: ${error.message}`);
